342675d7d1093cc24be044438b5ef7220ad70ea8
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-15 07:29:40 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿void IfWeekend(int day)
+{
+    if(day/6 >= 1) 
+        Console.WriteLine("Да");
+    else
+        Console.WriteLine("Нет");
+}
+
+int day = 6;
+IfWeekend(day);
+day = 7;
+IfWeekend(day);
+day = 1;
+IfWeekend(day);
